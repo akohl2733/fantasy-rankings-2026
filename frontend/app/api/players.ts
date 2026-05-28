c879a638d4a41f376924352694d26d7485aaa9fe
Andrew <akohl2733@gmail.com>
2026-05-27 23:39:06 -0400

    got first pass at instituting logging in frontend

--- frontend/app/api/players.ts
+++ frontend/app/api/players.ts
@@ -1,25 +1,32 @@
 import { Player } from "../components/PlayerCard";
+import { mainLogger } from "../lib/log";
 
 
 export async function fetchPlayers(){
+    const logger = mainLogger.getSubLogger({ name: 'fetchPlayers' })
     try {
         const res = await fetch("http://localhost:8000/players");
         if (!res.ok) {
             throw new Error(`There was an error on fetching: ${res.status}`);
         }
+
         const data = await res.json();
+
+        logger.info("Players fetched from backend", data);
+
         return data;
     }
     catch (e: any) {
-        console.error("There was an error:", e);
+        logger.error("Fetching of players failed", e);
     }
 };
 
 
 // Hit `/search_results` endpoint for getting player by similar name
 export async function getPlayersBySimilarName(playerName: string | null) {
+    const logger = mainLogger.getSubLogger({ name: 'getPlayersBySimilarName' });
     if (!playerName) {
-        console.log("No name provided");
+        logger.warn("No name provided to 'getPlayersBySimilarName'");
         return
     }
     try {
@@ -28,9 +35,12 @@ export async function getPlayersBySimilarName(playerName: string | null) {
             throw new Error(`There was an Error: ${res.status}`);
         }
         const playerList = await res.json();
+
+        logger.info("Players with similar names fetch successful", playerList);
         
         return playerList
-    } catch {
+    } catch (e: any) {
+        logger.error("Error fetching players by similar names", e);
         console.error("Error on fetching.");
     }
 }
